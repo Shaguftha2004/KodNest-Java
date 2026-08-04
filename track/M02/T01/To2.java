a0b9ce9b1a4e91fbb69fb03ba85adfa828fad761
Shaguftha2004 <2131shaguftha@gmail.com>
2026-08-05 00:00:19 +0530

    Day2changes

---
+++ track/M02/T01/To2.java
@@ -0,0 +1,10 @@
+
+public class To2 {
+
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+        System.out.println("Name " + learnerName);
+        System.out.println("Hours" + "Daily Practice: " + dailyPracticeHours + "Hours");
+    }
+}
