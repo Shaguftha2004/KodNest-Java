c9dea135bfc122b2b16a8d98d8bc962e01554cbc
Shaguftha2004 <2131shaguftha@gmail.com>
2026-08-04 12:34:22 +0530

    day3changes

---
+++ track/Practice.java/Main2.java
@@ -0,0 +1,16 @@
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        int num = 25;
+        if (num > 0) {
+            System.out.println("positive");
+        } else if (num > 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("Zero");
+        }
+
+    }
+
+}
