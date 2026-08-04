fcc5c388dc67475d07dd28b7b1838bee6f716853
Shaguftha2004 <2131shaguftha@gmail.com>
2026-08-04 21:16:33 +0530

    Day1changes

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/Main4.java
@@ -0,0 +1,10 @@
+
+public class Main4 {
+
+    public static void main(String[] args) {
+        // Display the students learning track.
+        System.out.println("track: Java Development");
+        //Display the student's daily commitment.
+        System.out.println("Daily Practice: 8 hours");
+    }
+}
