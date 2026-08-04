19522c893828abf332446da52bc167b4f2e1128b
Shaguftha2004 <2131shaguftha@gmail.com>
2026-08-04 11:46:42 +0530

    day3changes

---
+++ track/Practice.java/Main1.java
@@ -0,0 +1,13 @@
+
+public class Main1 {
+
+    public static void main(String arg[]) {
+        System.out.println("monday");
+        System.out.print("tuesday");
+        System.out.println("wednesday");
+        System.out.println("thursday");
+        System.out.print("friday");
+        System.out.println("saturday");
+        System.out.println("sunday");
+    }
+}
