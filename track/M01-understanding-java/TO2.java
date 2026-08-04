58d1c04def1b2e01c288c64a86650b34b67919ba
Shaguftha2004 <2131shaguftha@gmail.com>
2026-08-04 22:58:30 +0530

    Day2changes

---
+++ track/M01-understanding-java/TO2.java
@@ -0,0 +1,10 @@
+
+public class TO2 {
+
+    public static void main(String[] args) {
+        // Display repositary cloning status.
+        System.out.println("Repositary: Cloned Sucessfully");
+        //Display whether the project is ready.
+        System.out.println("Project Status: Ready for Development");
+    }
+}
