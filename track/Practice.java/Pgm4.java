17460e547ecece0eb8022f248aed13095db7e424
Shaguftha2004 <2131shaguftha@gmail.com>
2026-08-06 11:45:06 +0530

    Day5changes

---
+++ track/Practice.java/Pgm4.java
@@ -0,0 +1,27 @@
+
+public class Pgm4 {
+
+    public static void main(String[] args) {
+        int roundCode = 4;
+        int attemptsUsed = 2;
+        int maxAttempts = 3;
+        String roundName;
+        switch (roundCode) {
+            case 1:
+                roundName = "Aptitude Round";
+                break;
+            case 2:
+                roundName = "Technical Round";
+                break;
+            case 3:
+                roundNmae = "HR Round";
+                break;
+            case 4:
+            case 5:
+                roundName = "Final Review";
+            default
+
+        }
+
+    }
+}
