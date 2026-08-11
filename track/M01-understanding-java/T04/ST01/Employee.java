0fdc1610e4ed723ad5346446d8a6add2874c0a0a
Shaguftha2004 <2131shaguftha@gmail.com>
2026-08-11 11:20:02 +0530

    Employee

---
+++ track/M01-understanding-java/T04/ST01/Employee.java
@@ -0,0 +1,30 @@
+
+class EmpApplication {
+
+    int id;
+    String name;
+
+    void work() {
+        System.out.println("working");
+    }
+}
+
+public class Employee {
+
+    public static void main(String[] args) {
+        EmpApplication e1 = new EmpApplication();
+        e1.id = 11;
+        e1.name = "Anu";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        e1.work();
+        EmpApplication e2;
+        e2 = e1;
+        e2.id = 13;
+        e2.name = "Raju";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        System.out.println(e2.id);
+        System.out.println(e2.name);
+    }
+}
