658ffadd03c13f93973f8732ae5a69aadc1a884c
Shaguftha2004 <2131shaguftha@gmail.com>
2026-08-14 11:10:40 +0530

    StudentApp

---
+++ track/Practice.java/Main.java
@@ -0,0 +1,23 @@
+
+class MainString {
+
+    void m(String a, String b) {
+        System.out.println(a + b);
+    }
+
+    void m(String a, String b) {
+        System.out.println(a + b);
+    }
+
+    void m(A a, A b)
+}
+
+public class Main {
+
+    A a = new A();
+
+    a.m (
+
+null,null);
+
+}
