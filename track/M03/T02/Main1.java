e2200f67ff14408f9bfefac158defc017bf43dd8
Shaguftha2004 <2131shaguftha@gmail.com>
2026-08-31 15:53:39 +0530

    Daychanges

---
+++ track/M03/T02/Main1.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int a[] = new int[n];
+        int sorted = 1;
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                sorted = 0;
+                break;
+            }
+        }
+        System.out.println(sorted);
+
+    }
+}
