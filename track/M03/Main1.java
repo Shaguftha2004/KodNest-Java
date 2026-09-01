68b391fb72c46967a26e5bb0f0977bc2e0926718
Shaguftha2004 <2131shaguftha@gmail.com>
2026-09-01 15:30:54 +0530

    Clockwise

---
+++ track/M03/Main1.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int last = arr[n - 1];
+
+        for (int i = n - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+
+        System.out.println("Array after clockwise rotation:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
